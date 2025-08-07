674f87d2203372de432e84544db24f755113598a
Yixuan Zhang <zhangyixuan2002@gmail.com>
2025-08-08 01:52:15 +0800

    s

--- model_architectures/__init__.py
+++ model_architectures/__init__.py
@@ -1,16 +1,6 @@
 # model_architectures/__init__.py
 
-# 这样 Python 才会把这个文件夹当成一个包(package)来处理
+# 只导出 ResNet20，其他模块不要在这里导入
+from .resnet_cifar import ResNet20
 
-# 导出你要直接 import 的模块
-from .resnet_cifar    import ResNet20, ResNet32
-from .resnet_imagenet import Imagenet_ResNet20
-from .mobilenetv2     import MobileNetV2
-from .vgg             import vgg11, vgg13, vgg11_bn, vgg13_bn, vgg11_bn_sgm
-
-__all__ = [
-    "ResNet20", "ResNet32",
-    "Imagenet_ResNet20",
-    "MobileNetV2",
-    "vgg11", "vgg13", "vgg11_bn", "vgg13_bn", "vgg11_bn_sgm",
-]
+__all__ = ['ResNet20']
